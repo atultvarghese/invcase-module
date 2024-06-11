cff5ba5d7d29b1887a3aa9e241f368c07dbdb8f5
ajiththangavel <161320404+ajiththangavel@users.noreply.github.com>
2024-06-11 22:25:43 +0530

    Update carsenze.h

--- hardware/interfaces/carsenze/aidl/default/carsenze.h
+++ hardware/interfaces/carsenze/aidl/default/carsenze.h
@@ -1,25 +1,24 @@
+
 #pragma once
 
-
-#include "aidl/android/hardware/invcase/BnInvcase.h"
-
-
-
+#include <aidl/android/hardware/carsenze/BnCarsenze.h>
 
 namespace aidl {
-namespace android {
-namespace hardware {
-namespace invcase {
+    namespace android {
+        namespace hardware {
+            namespace carsenze {
 
-class Invcase : public BnInvcase {
-    public:
-        //String getChars();
-        ndk::ScopedAStatus getChars(std::string* _aidl_return);
-        //void putChars(in String msg);
-        ndk::ScopedAStatus putChars(const std::string& in_msg);
-};
+                class Carsenze : public BnCarsenze {
+                public:
+                    //String getMemoryStats();
+                    ndk::ScopedAStatus getMemoryStats(std::string* _aidl_return);
+                    //String getCpuStats();
+                    ndk::ScopedAStatus getCpuStats(std::string* _aidl_return);
+                    //String getNetworkStats();
+                    ndk::ScopedAStatus getNetworkStats(std::string* _aidl_return);
+                };
 
-}  // namespace invcase
-}  // namespace hardware
-}  // namespace android
+            }  // namespace carsenze
+        }  // namespace hardware
+    }  // namespace android
 }  // namespace aidl
