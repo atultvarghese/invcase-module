38a769793f05c7a732c2796b645e61d6689c1883
ajiththangavel <161320404+ajiththangavel@users.noreply.github.com>
2024-06-11 22:21:35 +0530

    Updated carsenze.cpp

--- hardware/interfaces/carsenze/aidl/default/carsenze.cpp
+++ hardware/interfaces/carsenze/aidl/default/carsenze.cpp
@@ -1,4 +1,4 @@
-#define LOG_TAG "Invcase"
+#define LOG_TAG "Carsenze"
 
 #include <utils/Log.h>
 #include <iostream>
@@ -6,41 +6,22 @@
 #include "carsenze.h"
 
 namespace aidl {
-namespace android {
-namespace hardware {
-namespace invcase {
+    namespace android {
+        namespace hardware {
+            namespace carsenze {
 
-//String getChars();
-ndk::ScopedAStatus Invcase::getChars(std::string* _aidl_return) {
-    std::ifstream invcase_dev;
-    invcase_dev.open("/dev/invcase");
-    if(invcase_dev.good()) {
-        std::string line;
-        invcase_dev >> line;
-        ALOGD("Invcase service: getChars: %s", line.c_str());
-        *_aidl_return =  line;
-    } else {
-        ALOGE("getChars: can not open /dev/invcase");
-        return ndk::ScopedAStatus::fromServiceSpecificError(-1);
-    }
-    return ndk::ScopedAStatus::ok();
-}
-
-//void putChars(in String msg);
-ndk::ScopedAStatus Invcase::putChars(const std::string& in_msg) {
-    std::ofstream invcase_dev;
-    invcase_dev.open ("/dev/invcase");
-    if(invcase_dev.good()) {
-        invcase_dev << in_msg;
-        ALOGD("Invcase service: putChars: %s", in_msg.c_str());
-    } else {
-        ALOGE("putChars: can not open /dev/invcase");
-        return ndk::ScopedAStatus::fromServiceSpecificError(-1);
-    }
-    return ndk::ScopedAStatus::ok();
-}
-
-}  // namespace invcase
-}  // namespace hardware
-}  // namespace android
-}  // namespace aidl
+//String getMemoryStats();
+                ndk::ScopedAStatus Carsenze::getMemoryStats(std::string* _aidl_return) {
+                    *_aidl_return =  "Memory = 1024 MB";
+                    return ndk::ScopedAStatus::ok();
+                }
+//String getCpuStats();
+                ndk::ScopedAStatus Carsenze::getCpuStats(std::string* _aidl_return) {
+                    *_aidl_return =  "CPU = 74.32 %";
+                    return ndk::ScopedAStatus::ok();
+                }
+//String getNetworkStats();
+                ndk::ScopedAStatus Carsenze::getCpuStats(std::string* _aidl_return) {
+                    *_aidl_return =  "Network = 10 Mb/s";
+                    return ndk::ScopedAStatus::ok();
+                }
